22d8c4b96ceaf882492260bdda0f941644c95a0f
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-04-13 22:15:28 -0400

    Add change 1 to ContentView

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Kode With Klossy!")
+            Text("Change 1")
         }
         .padding()
     }
